7b9804f07ea0904fc3062fbd70fa385afc5d01eb
Joao Gloria <jhfgloria@gmail.com>
2019-08-11 12:41:56 +0100

    start app

---
+++ src/main.go
@@ -0,0 +1,36 @@
+package src
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"os"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/auth/register")
+	router.POST("/auth/sign-in")
+	router.POST("/auth/logout")
+
+	router.GET("/jars")        // get all jars
+	router.POST("/jars")       // create jar
+	router.GET("/jars/:id")    // get one jar
+	router.PUT("/jars/:id")    // update one jar
+	router.DELETE("/jars/:id") // delete one jar
+	router.PATCH("/jars/:id")  // update jar's fine
+
+	router.POST("/jars/:id/user/:id") // fine user
+
+	router.POST("/jars/:id/invite") // invite user(s) to jar
+
+	router.POST("/users/:id") // pay specific fine per user
+
+	var port string
+	if os.Getenv("$PORT") == "" {
+		port = ":8000"
+	} else {
+		port = os.Getenv("$PORT")
+	}
+	log.Fatal(router.Run(port))
+}
